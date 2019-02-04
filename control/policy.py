46d3ceef29460e59882aa71b456906db679a0aec
alex-myzhao <mingyusysu@gmail.com>
2019-02-04 12:51:48 -0500

    purepursuit

--- control/policy.py
+++ control/policy.py
@@ -11,3 +11,19 @@ def adp(distance_2_tan, radian_at_tan, distance_integral, K):
     pwm_l_new = pwm_mid - differential_drive / 2
     pwm_r_new = pwm_mid + differential_drive / 2
     return pwm_l_new, pwm_r_new
+
+def pure_pursuit(distance_2_tan):
+    """Control with pure persuit method
+       L: distance between two wheel
+       alpha: angle error
+       l_d: distance from the vehicle to the center point
+    """
+    L = 8
+    l_d = 10
+    alpha = np.arcsin(distance_2_tan/l_d)
+    delta = np.arctan(2*L*np.sin(alpha)/l_d)
+    print('pure pursuit:', delta)
+    pwm_mid = 50.0
+    k = 5
+    pwm_l_new = pwm_mid - k * delta
+    pwm_r_new = pwm_mid + k * delta
